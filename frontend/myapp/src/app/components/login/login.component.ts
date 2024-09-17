bf16240d861ababe63dad4625b0a55f2ec13a8b7
liorosenman <lros209@gmail.com>
2024-09-17 12:38:20 +0300

    commit-8

--- frontend/myapp/src/app/components/login/login.component.ts
+++ frontend/myapp/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginsrvService } from '../../services/loginsrv.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,18 +9,45 @@ import { LoginsrvService } from '../../services/loginsrv.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  myForm: FormGroup;
   userInput: string = '';
   passInput: string = '';
   msg: string = '';
-  constructor(private loginSer: LoginsrvService) {}
-     
-  loginSubmit() {
-    const loginData = { username: this.userInput, password: this.passInput };  
-    this.loginSer.login(loginData).subscribe({
-      next: (msg) => console.log(msg),
-      error: (error) => console.error('Error during login:', error),
-      complete: () => console.log('Login request completed.')
+
+  constructor(private fb: FormBuilder, private loginsrvService: LoginsrvService) {
+    this.myForm = this.fb.group({
+      userInput: ['', Validators.required],
+      passInput: ['', Validators.required]
     });
   }
 
+  loginSubmit() {
+    if (this.myForm.valid) {
+      const credentials = this.myForm.value;
+      this.loginsrvService.login(credentials).subscribe({
+        next: (msg) => {
+          this.msg = 'Login successful!';
+        },
+        error: (err) => {
+          this.msg = 'Login failed. Please try again.';
+          console.error(err);
+        }
+      });
+    } else {
+      this.msg = 'Please fill in all required fields.';
+    }
+  }
 }
+  
+
+     
+  // loginSubmit() {
+  //   const loginData = { username: this.userInput, password: this.passInput };  
+  //   this.loginSer.login(loginData).subscribe({
+  //     next: (msg) => console.log(msg),
+  //     error: (error) => console.error('Error during login:', error),
+  //     complete: () => console.log('Login request completed.')
+  //   });
+  // }
+
+
